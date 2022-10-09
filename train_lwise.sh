00803ea13d062b6aafeada1a01f518eec1162fe6
ihounie <ihounie@gmail.com>
2022-10-09 18:02:51 -0400

    bash scripts

---
+++ train_lwise.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+{
+    BW=8
+    curr_dir="."
+    train_id="r_CE_PD_cifar10_resnet20_"
+    train_id="${train_id}${BW}"
+
+    result_dir="$curr_dir/results/$train_id"
+    mkdir -p $result_dir
+
+    python -u train_pd.py \
+        --model resnet20q \
+        --dataset cifar10 \
+        --train_split train \
+        --lr 0.001 \
+        --lr_dual 0.01 \
+        --lr_decay "100,150,180" \
+        --epochs 200 \
+        --optimizer adam \
+        --weight-decay 0.0 \
+        --results-dir $result_dir \
+        --bit_width_list "${BW}, 32"\
+        --wandb_log \
+        --layerwise_constraint \
+        --normalise_constraint \
+        --grads_wrt_high
+} && exit
