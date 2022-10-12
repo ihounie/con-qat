ce914dcf09741c3ade7b9dcc1fc52dc16add2e0b
ihounie <ihounie@gmail.com>
2022-10-12 00:57:40 -0400

    baselines

--- train_cifar10_baselines.sh
+++ train_cifar10_baselines.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-for bit_width in 32 8 4 2
+for seed in 0 1
+do
+for bit_width in 8 4 2 1
 do
 {
     curr_dir="."
-    train_id="baseline_cifar10_resnet20_${bit_width}"
+    train_id="baseline100_cifar10_resnet20_${bit_width}"
     train_stamp="$(date +"%Y%m%d_%H%M%S")"
     train_id=${train_id}_${train_stamp}
 
@@ -15,12 +17,14 @@ do
         --dataset cifar10 \
         --train_split train \
         --lr 0.001 \
-        --lr_decay "100,150,180" \
-        --epochs 200 \
+        --lr_decay "50,75,90" \
+        --epochs 100 \
         --optimizer adam \
         --weight-decay 0.0 \
         --results-dir $result_dir \
-        --bit_width_list "${bit_width}"\
+        --bit_width_list "${bit_width}, 32"\
         --wandb_log
+        --seed "${seed}"
 }
+done
 done
