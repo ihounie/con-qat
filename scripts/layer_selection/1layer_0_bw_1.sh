9a1550971a10e4c3874413aadd90347cf47d644b
ihounie <ihounie@fing.edu.uy>
2022-10-23 09:58:28 -0400

    1 layer exp

---
+++ scripts/layer_selection/1layer_0_bw_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+            for seed in 0 1 2
+            do
+                curr_dir="."
+                train_id="1l_1d_1_${seed}"
+                result_dir="./results/$train_id"
+                mkdir -p $result_dir
+                python -u train_pd_layers_selec.py \
+                    --model resnet20q \
+                    --dataset cifar10 \
+                    --train_split train \
+                    --lr 0.001 \
+                    --lr_decay "50,75,90" \
+                    --epochs 100 \
+                    --optimizer adam \
+                    --weight-decay 0.0 \
+                    --results-dir $result_dir \
+                    --bit_width_list "1" \
+                    --no_quant_layer "['7conv1']" \
+                    --seed "${seed}" \
+                    --wandb_log \
+                    --epsilonlw 0.7 \
+                    --project QSLayerRetrain
+            done
+            
