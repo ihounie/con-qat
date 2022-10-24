5d448a1d53988cbde3621406587b2fcc2337c51b
ihounie <ihounie@fing.edu.uy>
2022-10-23 22:11:38 -0400

    16 bit

---
+++ scripts/layer_selection/best_16_bw_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+            for seed in 0 1 2
+            do
+                curr_dir="."
+                train_id="16l_bestd_1_${seed}"
+                result_dir="./results/$train_id"
+                mkdir -p $result_dir
+                python -u train_pd_layers_selec.py \
+                    --model resnet20q \
+                    --dataset cifar10 \
+                    --train_split train \
+                    --lr 0.001 \
+                    --lr_decay "50,75,90" \
+                    --epochs 100 \
+                    --optimizer adam \
+                    --weight-decay 0.0 \
+                    --results-dir $result_dir \
+                    --bit_width_list "1" \
+                    --no_quant_layer "Block_2_conv1,Block_4_conv1,Block_1_conv1,Block_8_conv0,Block_6_conv1,Block_0_conv1,Block_5_conv0,Block_7_conv0,Block_2_conv0,Block_6_conv0,Block_4_conv0,Block_3_conv0,Block_1_conv0,Block_0_conv0,Block_6_shortcut,Block_3_shortcut" \
+                    --seed "${seed}" \
+                    --wandb_log \
+                    --epsilonlw 0.7 \
+                    --project QSLayerRetrain
+            done
+            
