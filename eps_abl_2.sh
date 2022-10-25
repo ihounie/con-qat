97e2e54ef5227275bbb143b4e91aaa919583566f
ihounie <ihounie@gmail.com>
2022-10-25 01:20:42 -0400

    eps

---
+++ eps_abl_2.sh
@@ -0,0 +1,12 @@
+BW=8
+EPSILON=$1
+for SEED in 0 1 2
+do
+        train_id="${BW}_${EPSILON}"
+        result_dir="./results/$train_id"
+        mkdir -p $result_dir
+
+        python -u train_pd_layers.py --epsilon_out 0.1 --seed $SEED --model resnet20q --dataset cifar10 --train_split train --lr 0.001 \
+            --lr_decay "50,75,100" --epochs 100 --optimizer adam --weight-decay 0.0 --results-dir $result_dir \
+            --bit_width_list "${BW}, 32" --epsilonlw $EPSILON --layerwise_constraint --wandb_log --project L2AblationEpsilon --constraint_norm L2
+done
